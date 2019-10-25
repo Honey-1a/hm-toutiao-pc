9c14b1a55a39d3b2009765cb0189c6696964d754
Honey-1a <1795109806@qq.com>
2019-10-25 22:34:56 +0800

    登录模块基础布局和绘制表单

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div id="app">123
-    <el-button type="success">成功按钮</el-button>
+  <div id="app">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'app'
+}
 </script>
 
 <style lang="less">
